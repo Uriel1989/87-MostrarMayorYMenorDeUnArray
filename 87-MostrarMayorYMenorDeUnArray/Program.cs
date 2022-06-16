d8b3aa40f7ca9f80706b25af2708600e3024adcd
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:55:13 -0300

    Add files via upload

---
+++ 87-MostrarMayorYMenorDeUnArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Dado un array de números, muestra el mayor y el menor.
+
+namespace _87_MostrarMayorYMenorDeUnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = { 4, 3, 7, 10, 100, 5, 7 };
+
+            int maximo = valores[0];
+            int minimo = valores[0];
+
+            for (int i = 1; i < valores.Length; i++)
+            {
+                if (valores[i] > maximo)
+                {
+                    maximo = valores[i];
+                }
+
+                if (valores[i] < minimo)
+                {
+                    minimo = valores[i];
+                }
+
+            }
+
+            Console.WriteLine("El valor minimo es: " + minimo);
+            Console.WriteLine("El valor maximo es: " + maximo);
+        }
+    }
+}
